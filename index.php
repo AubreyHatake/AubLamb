9fde07f8c292d25dfb66e3e9c1f5fc05520c85d9
Aubrey Lamb <aubreyrlamb@gmail.com>
2024-01-13 18:17:21 -0700

    done

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<h1>This is Jeff Stone's PHP page</h1>";
+echo "<p>Someday, this page will be an awesome page!</p>";
+echo "<a href=index.php>This link will help Jeff grade this assignment</a> :0]";
+?>
